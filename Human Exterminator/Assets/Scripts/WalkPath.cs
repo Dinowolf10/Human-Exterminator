d3a191bbb2bfdb8352d7cbef56755a946f52c59a
Ethan Ball <42217726+Starchy29@users.noreply.github.com>
2021-10-03 14:35:21 -0400

    Added script that moves an enemy along a path

---
+++ Human Exterminator/Assets/Scripts/WalkPath.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Moves an entity around a path going towards one point at a time
+public class WalkPath : MonoBehaviour
+{
+    private int nextPoint = 0; // the index of the next point to get to
+    private float waitTime = 0; // tracks how much the entity has waited
+
+    public int waitTimeAtPivots = 0; // the amount of time the entity pauses when they get to a point
+    public float walkSpeed = 10f; // how fast this entity will move along its path
+    public List<Vector3> path = new List<Vector3>(); // Add points to this list to create the path. Order matters
+
+    // Start is called before the first frame update
+    void Start() {}
+
+    // Moves the entity towards the next point in the path. Once they pass it, they move to the next point
+    void Update()
+    {
+        if(path.Count <= 0) {
+            return;
+        }
+
+        // wait at a point
+        if(waitTime > 0) {
+            waitTime -= Time.deltaTime;
+            return;
+        }
+
+        // calculate direction to move
+        Vector3 target = path[nextPoint];
+        Vector3 direction = (target - transform.position).normalized;
+
+        // move in that direction
+        transform.Translate(direction * walkSpeed * Time.deltaTime);
+
+        // check if target point was reached
+        Vector3 directionToTarget = target - transform.position;
+        if(Vector3.Dot(directionToTarget, direction) <= 0) {
+            // correct position to be on the point
+            transform.position = path[nextPoint];
+
+            // start moving to the next point
+            nextPoint++;
+            if(nextPoint > path.Count - 1) {
+                nextPoint = 0;
+            }
+
+            // wait the desired amount first
+            waitTime = waitTimeAtPivots;
+        }
+    }
+}
