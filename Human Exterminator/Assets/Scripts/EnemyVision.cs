31a423556190bc40d0b6b9fe01a98d620c81c78c
Zaven <dinowolf10@gmail.com>
2021-10-28 23:20:46 -0400

    Fixed enemy vision detection and wall collision problems with phasing

--- Human Exterminator/Assets/Scripts/EnemyVision.cs
+++ Human Exterminator/Assets/Scripts/EnemyVision.cs
@@ -6,7 +6,10 @@ using UnityEngine.SceneManagement;
 public class EnemyVision : MonoBehaviour
 {
     [SerializeField]
-    private float maxVision = 5.0f;
+    private float maxVision = 10.0f;
+
+    [SerializeField]
+    private GameObject enemy;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -15,23 +18,38 @@ public class EnemyVision : MonoBehaviour
         if (collision.gameObject.tag == "Player" && !collision.gameObject.GetComponent<CharacterMovement>().isDying)
         {
             // Call the PlayerDeath method
-            collision.gameObject.GetComponent<CharacterMovement>().StartCoroutine("PlayerDeath");
+            //collision.gameObject.GetComponent<CharacterMovement>().StartCoroutine("PlayerDeath");
 
-            /*// Disable player
-            collision.gameObject.SetActive(false);
+            Vector2 direction = (collision.transform.position - enemy.transform.position).normalized;
 
-            // Restart the current scene
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-            */
+            // Shoots a raycast from this enemy's position to the player's position
+            RaycastHit2D hit = Physics2D.Raycast(enemy.transform.position, direction, maxVision);
+
+            // Draws the raycast as a debug line
+            Debug.DrawLine(enemy.transform.position, direction * maxVision, Color.red);
+
+            // If there was a collider hit
+            if (hit.collider != null)
+            {
+                Debug.Log("Hit " + hit.collider.gameObject.tag);
+
+                // Check if the collider hit was the player
+                // If it was the player, disable the player and restart the current scene
+                if (hit.collider.gameObject.tag == "Player")
+                {
+                    // Call the PlayerDeath method
+                    collision.gameObject.GetComponent<CharacterMovement>().StartCoroutine("PlayerDeath");
+                }
+            }
 
             // Checks for obstacles in between enemy and player
             //CheckForObstacles(collision.gameObject);
         }
     }
 
-    /*private void OnTriggerStay2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        // Checks if the colliding object is the player
+        /*// Checks if the colliding object is the player
         // If it is the player, call the CheckForObstacles method
         if (collision.gameObject.tag == "Player")
         {
@@ -43,11 +61,42 @@ public class EnemyVision : MonoBehaviour
 
             // Checks for obstacles in between enemy and player
             //CheckForObstacles(collision.gameObject);
-        }
-        
-    }*/
+        }*/
 
-    /// <summary>
+        if (collision.gameObject.tag == "Player" && !collision.gameObject.GetComponent<CharacterMovement>().isDying)
+        {
+            // Call the PlayerDeath method
+            //collision.gameObject.GetComponent<CharacterMovement>().StartCoroutine("PlayerDeath");
+
+            Vector2 direction = (collision.transform.position - enemy.transform.position).normalized;
+
+            // Shoots a raycast from this enemy's position to the player's position
+            RaycastHit2D hit = Physics2D.Raycast(enemy.transform.position, direction, maxVision);
+
+            // Draws the raycast as a debug line
+            Debug.DrawLine(enemy.transform.position, direction * maxVision, Color.red);
+
+            // If there was a collider hit
+            if (hit.collider != null)
+            {
+                Debug.Log("Hit " + hit.collider.gameObject.tag);
+
+                // Check if the collider hit was the player
+                // If it was the player, disable the player and restart the current scene
+                if (hit.collider.gameObject.tag == "Player")
+                {
+                    // Call the PlayerDeath method
+                    collision.gameObject.GetComponent<CharacterMovement>().StartCoroutine("PlayerDeath");
+                }
+            }
+
+            // Checks for obstacles in between enemy and player
+            //CheckForObstacles(collision.gameObject);
+        }
+
+    }
+
+    /*/// <summary>
     /// Checks for obstacles in between the enemy and player
     /// </summary>
     /// <param name="collision"></param>
@@ -75,5 +124,5 @@ public class EnemyVision : MonoBehaviour
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
             }
         }
-    }
+    }*/
 }
